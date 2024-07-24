581a9ab3c0fe0328abcfc8e8cea8fdbf646568d7
ktrlsrfn <fwidjaja15@gmail.com>
2024-07-24 21:03:53 +0700

    update: added task looping logic

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,7 +1,50 @@
+import { useState } from 'react';
 import logo from './happy5-logo.png';
 import Taskbox from './TaskBox';
 
 function Dashboard() {
+  const [tasks, setTasks] = useState([
+    {
+      date: 'Q1 2019',
+      month: 'January - March',
+      tasks: [
+        {
+          title: 'Re-designed the zero-g doggie bags. No more spills!',
+          percent: 64
+        },
+        {
+          title: 'Travel & Relocation Support',
+          percent: 12
+        }
+      ]
+    },
+    {
+      date: 'Q2 2019',
+      month: 'April - June',
+      tasks: []
+    },
+    {
+      date: 'Q3 2019',
+      month: 'July - September',
+      tasks: [
+        {
+          title: 'Bundle interplanetary analytics for improved transmission',
+          percent: 90
+        },
+      ]
+    },
+    {
+      date: 'Q4 2019',
+      month: 'October - December',
+      tasks: [
+        {
+          title: 'Data Migration: Performance & Culture End Game',
+          percent: 63
+        },
+      ]
+    },
+  ]);
+
   return (
     <div className="flex">
       <div className="flex flex-col items-center bg-[#1a0931] h-screen w-[68px] py-[20px]">
@@ -10,7 +53,7 @@ function Dashboard() {
       <div className="text-left px-[50px] pt-[20px] space-y-6">
         <h3 className="text-xl">Product Roadmap</h3>
         <div className="flex items-start gap-4">
-          <Taskbox />
+          {tasks.map(item => (<Taskbox date={item.date} month={item.month} tasks={item.tasks} />))}
         </div>
       </div>
     </div>
--- src/TaskBox.jsx
+++ src/TaskBox.jsx
@@ -1,19 +1,15 @@
 import plus from './plus-icon.svg';
 import TaskItem from './TaskItem';
 
-function TaskBox() {
+function TaskBox({ date, month, tasks }) {
   return (
-    <div className="flex w-[306px] flex-col items-start px-[12px] text-left rounded-md border-[1px]">
+    <div className="flex w-[306px] flex-col items-start px-[12px] text-left rounded-md border-[1px] pt-2">
       <div className="py-2">
-        <p className="text-[#5D6372] font-normal text-[12px]">Q1 2019</p>
-        <p className="text-[#5D6372] text-[14px]">January - March</p>
+        <p className="text-[#5D6372] font-normal text-[12px]">{date}</p>
+        <p className="text-[#5D6372] text-[14px]">{month}</p>
       </div>
       <div className="py-2 space-y-2">
-        <TaskItem
-          title="Re-designed the zero-g doggie bags. No more spills!"
-          percent="64"
-        />
-        <TaskItem title="Travel & Relocation Support" percent="12" />
+        {tasks.length === 0 ? <div className="text-[#5D6372] text-[14px] font-normal">No Tasks Available</div> : tasks.map(task => (<TaskItem title={task.title} percent={task.percent} />))}
       </div>
       <div className="flex items-center w-full py-4 pt-2">
         <img src={plus} alt="" className="w-3 h-3 " />
